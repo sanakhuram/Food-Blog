c9f8cd3a3ee41ade65bc36d7bd14d1e4bb45fd33
Sana Khuram <126725476+sanakhuram@users.noreply.github.com>
2024-05-21 17:38:04 +0200

    added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./**/*.html'], // Adjust the paths as needed
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
